f6d90e0c5464492495216369a6c7f499e2b458a6
Alessia Lescano <alessiaklescano@gmail.com>
2025-02-28 18:48:25 -0300

    Updated bot.py
    Corregido errores que no permitían que el bot encontrase los archivos .pdf de la página web debido a cambios en ésta última.
    Optimizado el código para que no hayan repeticiones innecesarias y sea más fácil de leer.

--- bot.py
+++ bot.py
@@ -4,18 +4,19 @@ import requests
 import asyncio
 
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv, set_key
+from discord import app_commands
 from discord.ext import commands
+from dotenv import load_dotenv, set_key
 from datetime import datetime
 
 env_directorio = "bot.env"
 load_dotenv(env_directorio)
 
-TOKEN = os.environ.get('DISCORD_TOKEN')
-LINK_MATERIAS = os.environ.get('LINK_MATERIAS')
-LINK_FINALES = os.environ.get('LINK_FINALES')
-FECHA_MATERIAS = os.environ.get('MATERIAS_FECHA_GUARDADA')
-FECHA_FINALES = os.environ.get('FINALES_FECHA_GUARDADA')
+TOKEN = os.getenv('DISCORD_TOKEN')
+LINK_MATERIAS = os.getenv('LINK_MATERIAS')
+LINK_FINALES = os.getenv('LINK_FINALES')
+FECHA_MATERIAS = os.getenv('MATERIAS_FECHA_GUARDADA')
+FECHA_FINALES = os.getenv('FINALES_FECHA_GUARDADA')
 UNGS_LINK = 'https://www.ungs.edu.ar/estudiar-en-la-ungs/inscripciones/oferta-academica-mesas-de-examen'
 
 if TOKEN is None:
@@ -25,7 +26,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('/'), intents=intents)
+bot = commands.Bot(command_prefix="$", intents = discord.Intents.all())
 
 def setear_id_canal(id_canal):
     id_canal_str = str(id_canal)
@@ -43,145 +44,111 @@ def get_channel_by_id(client, id_canal):
         canal = guild.get_channel(id_canal)
         if canal:
             return canal
-    return None
+    return None    
 
-async def chequearActualizacion(TEXT):
+async def fetch_pdf_link(keyword):
     global UNGS_LINK
-    response = requests.get(UNGS_LINK)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        def tiene_palabra(string):
-            return string and TEXT in string
-
-        link_tag = soup.find('a', string=tiene_palabra)
-
-        if link_tag:
-            href = link_tag['href']
-            print("Link al PDF:", href)
-            return href
-        else:
-            print(f'Link no encontrado.')
-    else:
-        print(f'Fallo al buscar la página web.')
-
-async def chequearOferta():
-    global LINK_MATERIAS
-    global FECHA_MATERIAS
-    
-    texto = 'Ver Oferta Académica'
-    NuevoLink = ''
-    NuevoLink = await chequearActualizacion(texto)
-
-    id_canal_notificar = get_id_canal()
-    if id_canal_notificar != 0:
-        canal = get_channel_by_id(bot, id_canal_notificar)
-
-    if NuevoLink != LINK_MATERIAS:
-        mensaje = f'@everyone ¡El pdf de Oferta Académica fue actualizado! Aquí está el nuevo enlace: {NuevoLink}. No olvides anotarte antes que se termine el llamado de este semestre~ (Este mensaje puede reenviarse varias veces, depende de la cantidad de actualizaciones que hagan durante las fechas de inscripción.)'
-
-        if canal:
-            LINK_MATERIAS = NuevoLink
-            fecha_actual = datetime.now()
-
-            formatofecha = fecha_actual.strftime("%d-%m-%Y %H:%M:%S")
-            FECHA_MATERIAS = formatofecha
-
-            set_key('bot.env', 'LINK_MATERIAS', LINK_MATERIAS)
-            set_key('bot.env', 'MATERIAS_FECHA_GUARDADA', FECHA_MATERIAS)
-
-            print(f'El link almacenado era distinto al de la página web, el nuevo link se ha almacenado!')
-            await canal.send(mensaje)
-        else:
-            print("El ID del canal no ha sido seteado aún! Usa el comando '@{bot.user} set_channel' para setear el ID.")
-    else:
-        print(f'El link almacenado tiene la última versión.')
-        fecha_actual = datetime.now()
-
-        formatofecha = fecha_actual.strftime("%d-%m-%Y %H:%M:%S")
-        FECHA_MATERIAS = formatofecha
-
-        set_key('bot.env', 'MATERIAS_FECHA_GUARDADA', FECHA_MATERIAS)
-        if canal: 
-            await canal.send(f"La última versión del calendario es la siguiente: {LINK_MATERIAS}.")
-        else:
-            print ("Hubo un error al intentar enviar el mensaje al canal.")
-
-async def chequearFinal():
-    global LINK_FINALES
-    global FECHA_FINALES
-    texto = 'Ver Mesas de Examen'
-    NuevoLink = ''
-    NuevoLink = await chequearActualizacion(texto)
-
-    id_canal_notificar = get_id_canal()
-    if id_canal_notificar != 0:
-        canal = get_channel_by_id(bot, id_canal_notificar)
-
-    if NuevoLink != LINK_FINALES:
-        mensaje = f'@everyone ¡El pdf de las Mesas de Examen fue actualizado! Aquí está el nuevo enlace: {NuevoLink}.)'
-
-        if canal:
-            LINK_FINALES = NuevoLink
-            fecha_actual = datetime.now()
-
-            formatofecha = fecha_actual.strftime("%d-%m-%Y %H:%M:%S")
-            FECHA_FINALES = formatofecha
-
-            set_key('bot.env', 'LINK_FINALES', LINK_FINALES)
-            set_key('bot.env', 'FINALES_FECHA_GUARDADA', FECHA_FINALES)
-
-            print(f'El link almacenado era distinto al de la página web, el nuevo link se ha almacenado!')
-            await canal.send(mensaje)
-        else:
-            print("El ID del canal no ha sido seteado aún! Usa el comando '@{bot.user} set_channel' para setear el ID.")
-    else:
-        print(f'El link almacenado tiene la última versión.')
-        fecha_actual = datetime.now()
-        	
-        formatofecha = fecha_actual.strftime("%d-%m-%Y %H:%M:%S")
-        FECHA_FINALES = formatofecha
+    try:
+        response = requests.get(UNGS_LINK)
         
-        set_key('bot.env', 'FINALES_FECHA_GUARDADA', FECHA_FINALES)
-        if canal: 
-            await canal.send(f"La última versión de la oferta a finales es la siguiente: {LINK_FINALES}.")
-        else:
-            print ("Hubo un error al intentar enviar el mensaje al canal.")
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            
+            link_tag = soup.find('a', href=lambda href:href and keyword in href)
 
+            if link_tag:
+                href = link_tag['href']
+                print("Link al PDF:", href)
+                return href
+            else: 
+                return None
+        else:
+            print(f"No se encontró la página.")
+    except requests.RequestException as e:
+        print(f"Error buscando PDF: {e}")
+        return None
+            
+async def chequearOferta(keyword: str):
+    global LINK_MATERIAS, FECHA_MATERIAS, LINK_FINALES, FECHA_FINALES
+    
+    links = {
+        "Publicacion-Materias": {"link": LINK_MATERIAS, "fecha": FECHA_MATERIAS, "env_key": "LINK_MATERIAS", "date_key": "MATERIAS_FECHA_GUARDADA"},
+        "MESAS-DE-EXAMEN": {"link": LINK_FINALES, "fecha": FECHA_FINALES, "env_key": "LINK_FINALES", "date_key": "FINALES_FECHA_GUARDADA"}
+    }
+    
+    if keyword not in links:
+        print(f"Keyword '{keyword}' no válido.")
+        return
+        
+    nuevoLink = await fetch_pdf_link(keyword)
+    
+    if not nuevoLink:
+        print(f"No se encontró el archivo.")
+        return
+    
+    link_info = links[keyword]
+    
+    id_canal_notificar = get_id_canal()
+    canal = get_channel_by_id(bot, id_canal_notificar) if id_canal_notificar else None
+    
+    if nuevoLink != link_info["link"]:
+        if (keyword == "Publicacion-Materias"):
+            mensaje = f'@everyone ¡El PDF de Oferta Académica fue actualizado! Aquí está el nuevo enlace: {nuevoLink}. No olvides anotarte antes que se termine el llamado~'
+        elif (keyword == "MESAS-DE-EXAMEN"):
+            mensaje = f'@everyone ¡El pdf de las Mesas de Examen fue actualizado! Aquí está el nuevo enlace: {nuevoLink}.'
+
+        if canal:
+            link_info["link"] = nuevoLink
+            fecha_actual = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+            link_info["fecha"] = fecha_actual
+
+            set_key('bot.env', link_info["env_key"], nuevoLink)
+            set_key('bot.env', link_info["date_key"], fecha_actual)
+
+            print(f'El link almacenado era distinto al de la página web, el nuevo link se ha almacenado!')
+            await canal.send(mensaje)
+        else:
+            print("El ID del canal no ha sido seteado aún! Usa el comando '@{bot.user} set_channel' para setear el ID.")
+    else:
+        print(f'El link almacenado tiene la última versión.')
+        if canal:
+            await canal.send(f'El link almacenado tiene la última versión. Usa /ultimoLink para ver los links a las materias.')
+    
 @bot.event
 async def on_ready():
-    print(f'{bot.user} se ha conectado a Discord!')
-    await chequearOferta()
-    await chequearFinal()
+    print(f'Bot is up and ready!')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} comandos sincronizados")
+    except Exception as e:
+        print(f" Error al sincronizar comandos: {e}")
+        
+    await chequearOferta("Publicacion-Materias")
+    await chequearOferta("MESAS-DE-EXAMEN")
 
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send(
-        f'Hola {member.name}, bienvenido al servidor! Como aviso, tenemos un canal notificacion-inscripcion donde '
-        f'recibirás notificaciones semestrales cuando sea fecha de inscripción por mi parte. Espero que te '
-        f'diviertas, y si te molesto mucho puedes mutear el canal.'
-    )
-
-@bot.command()
-async def set_channel(ctx, id_canal: discord.TextChannel):
+    await member.dm_channel.send(f"Hola {member.name}, bienvenido al servidor! Como aviso, tenemos un canal notificacion-inscripcion donde recibirás notificaciones semestrales cuando sea fecha de inscripción por mi parte. Espero que te diviertas, y si te molesto mucho puedes mutear el canal.")
+        
+@bot.tree.command(name="setcanal", description="Setea el canal donde el bot enviará los mensajes")
+async def set_channel(interaction: discord.Interaction, id_canal: discord.TextChannel):
     setear_id_canal(id_canal.id)
-    await ctx.send(f'El canal {id_canal.mention} se ha seteado. Los mensajes se enviarán a este canal.')
+    await interaction.response.send_message(f"{interaction.user.mention} el canal {id_canal.mention} se ha seteado. Los mensajes se enviarán a este canal.", ephemeral=True)
 
-@bot.command()
-async def chequearMaterias(ctx):
-    await chequearOferta()
-    await ctx.send('Chequeo de calendario de Materias exitoso.')
+@bot.tree.command(name="chequearoferta", description="Verifica que el calendario de oferta académica esté al día")
+async def chequearOfertaAcademica(interaction: discord.Interaction):
+    await chequearOferta("Publicacion-Materias")
+    await interaction.response.send_message("Chequeo de calendario de Materias exitoso.", ephemeral=True)
+    
+@bot.tree.command(name="chequearfinales", description="Verifica que el calendario de mesa de examen esté al día")
+async def chequearFinal(interaction: discord.Interaction):
+    await chequearOferta("MESAS-DE-EXAMEN")
+    await interaction.response.send_message("Chequeo de calendario de Finales exitoso.", ephemeral=True)
 
-@bot.command()
-async def chequearFinales(ctx):
-    await chequearFinal()
-    await ctx.send('Chequeo de calendario de Finales exitoso.')
-
-@bot.command()
-async def ultimoRegistro(ctx):
-    await ctx.send(f'La última versión que poseo de la Oferta Académica es: {FECHA_MATERIAS}, y el link es: {LINK_MATERIAS}. La última versión que poseo de las Mesas de Examen es: {FECHA_FINALES}, y el link es: {LINK_FINALES}.')
+@bot.tree.command(name="ultimolink", description="¡Te muestra el último link que tiene guardado al bot sin pingear a todos en el server!")
+async def ultimoRegistro(interaction: discord.Interaction):
+    await interaction.response.send_message(f'{interaction.user.mention} la última versión que poseo de la Oferta Académica es del: {FECHA_MATERIAS}, y el link es: {LINK_MATERIAS}.\nLa última versión que poseo de las Mesas de Examen es: {FECHA_FINALES}, y el link es: {LINK_FINALES}.')
 
 bot.run(TOKEN)
 
